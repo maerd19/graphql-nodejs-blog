4fa163373febf4fab26fd632eaec4f18a6225702
Alejandro Hernandez <alejandro.hernandez@yalochat.com>
2022-01-17 20:07:04 -0600

    Users Query

--- graphql/queries.js
+++ graphql/queries.js
@@ -1,9 +1,12 @@
-const {GraphQLString} = require('graphql')
+const { GraphQLList } = require('graphql') 
+const { UserType } = require('./types')
+const { User } = require('../models')
 
-const hello = {
-    type: GraphQLString,
-    description: 'Returs a string',
-    resolve: () => 'Hello world'   
+const users = {
+    type: new GraphQLList(UserType),
+    resolve() {
+        return User.find()
+    }
 }
 
-module.exports = {hello}
+module.exports = { users }
